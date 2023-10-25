db073f35de76f50641411d8f2e9f625abb4d19d2
cwscarlos <carlos03dsa@hotmail.com>
2023-10-25 16:04:02 -0600

    Add files via upload

---
+++ server/constants.js
@@ -0,0 +1,17 @@
+const DB_USER ="admin";
+const DB_PASSWORD ="admin123456";
+const DB_HOST ="cluster0.p6pvgyw.mongodb.net";
+
+const API_VERSION ="v1";
+const IP_SERVER ="localhost";
+
+const JWT_SECRET_KEY="lkj34234ljk23k4j23";
+
+module.exports={
+	DB_USER,
+	DB_PASSWORD,
+	DB_HOST,
+	IP_SERVER,
+	API_VERSION,
+	JWT_SECRET_KEY,
+}
